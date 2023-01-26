e3ef23245470d42b14860d68204b5f7ed9c0ed3c
Zayan <mohammadzayan347@gmail.com>
2023-01-27 00:01:32 +0530

    Added functionality of add route

--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,14 @@
 const router = require("express").Router()
 const bodyParser = require("body-parser")
+const fs = require("fs")
+const path = require("path")
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 router.post("/add", urlencodedParser, (req, res)=>{
-    console.log(req.body)
-    res.json("this is add route")
+    const data = JSON.stringify(req.body)
+    fs.writeFileSync(path.join(__dirname,"../data/words.txt"), data)
+    res.status(200).json("this is add route")
 })
 
 module.exports = router
