7f098f4967dfeccc22d5bf8553308c231248039b
Zayan <mohammadzayan347@gmail.com>
2023-01-27 12:02:59 +0530

    Add route backend completed (may require some cleaning of the code)

--- routes/router.js
+++ routes/router.js
@@ -11,18 +11,28 @@ router.post("/add", urlencodedParser, (req, res)=>{
     if(isDataFile){
         let words = fs.readFileSync(path.join(__dirname, "../data/words.txt")).toString()
         if(words.includes(req.body.word)){
-            let wordProperties = words.split("?")
+            let wordProperties = JSON.parse(words)
             for (let i = 0; i < wordProperties.length; i++) {
                 const e = wordProperties[i];
                 try {
-                    wordProp = JSON.parse(e).value
-                    if(wordProp == req.body.value){
-                     return res.status(200).json("Word already exists")
-                     break
-                    }
-                    else{
-                        wordProp = req.body.value
-                        console.log(wordProp)
+                    wordProp = e.value
+                    if(e.word== req.body.word){
+                        if(wordProp == req.body.value){
+                            return res.status(200).json("Word already exists")
+                            break
+                           }
+                           else{
+                               wordProp = req.body.value
+                               e.value = req.body.value
+                                let newData = JSON.parse(words)
+                                newData.forEach(element => {
+                                    if(element.word == req.body.word){
+                                        element.value = req.body.value
+                                    }
+                                });
+                                fs.writeFileSync(path.join(__dirname, "../data/words.txt"), JSON.stringify(newData))
+                                res.status(200).json("Word updated")
+                           }
                     }
                     
                 } catch (error) {
@@ -30,14 +40,17 @@ router.post("/add", urlencodedParser, (req, res)=>{
                 }
             }
         }
-            else{
-            // return res.status(200).json("Word Already there")
+        else{
+            let newData = JSON.parse(words)
+            newData.push(JSON.parse(JSON.stringify(req.body)))
+            fs.writeFileSync(path.join(__dirname, "../data/words.txt"), JSON.stringify(newData))
+            res.status(200).json("Word added")
         }
     }
- else{
-    fs.writeFileSync(path.join(__dirname,"../data/words.txt"), data+ "?")
-    return res.status(200).json("this is add route")
- }
+    else{
+        fs.writeFileSync(path.join(__dirname,"../data/words.txt"), "["+data+ "]")
+        return res.status(200).json("this is add route")        }
+
 })
 
 module.exports = router
